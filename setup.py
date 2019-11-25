b503a8f147f22e19a8788888e587b7656e4400fc
Feng Hao <hao.feng@hpe.com>
2019-11-25 13:17:31 +0800

    django package structure

---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding:utf-8 -*-
+# This Python file uses the following encoding: utf-8
+from setuptools import setup, find_packages
+import os
+import sys
+
+
+# Get description from Readme file
+readme_file = os.path.join(os.path.dirname(__file__), 'README.md')
+long_description = open(readme_file).read()
+
+# Build a list with requirements of the app
+REQUIRES = []
+try:
+    import django
+except ImportError:
+    # Because of the strange update behavior of "pip --upgrade package_name"
+    # set requierment only if django not avallible.
+    REQUIRES.append('django')
+
+if sys.version_info < (3, 4):
+   REQUIRES.append('python >= 3.4')
+
+
+setup(name='django-onetimelink',
+        version=0.1,
+        description='A django file streaming application',
+        long_description=long_description,
+        author='FENG Hao',
+        author_email='hiroshifuu@outlook.com',
+        url='',
+        download_url='https://pypi.python.org/pypi/django-onetimelink',
+        license='BSD',
+        packages=find_packages(exclude=['example', ]),
+        include_package_data=True,
+        keywords="django one-time file link serve",
+        classifiers=[
+              'Development Status :: 5 - Production/Stable',
+              'Framework :: Django',
+              'License :: OSI Approved :: BSD License',
+              'Operating System :: OS Independent',
+              'Programming Language :: Python :: 3 :: Only',
+              'Environment :: Console',
+              'Natural Language :: English',
+              'Intended Audience :: Developers',
+              'Topic :: Internet',
+              ],
+        install_requires=REQUIRES,
+        zip_safe=False,
+        )
